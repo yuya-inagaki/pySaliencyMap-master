800f42e0e6f809eb151942ac37b8eae88640047c
Yuya Inagaki <yuya1756@live.jp>
2019-07-13 23:28:24 +0900

    update

--- test_get_element_bytext.py
+++ test_get_element_bytext.py
@@ -18,6 +18,13 @@ import MeCab
 import re
 from collections import Counter
 
+#csvに使用
+import csv
+
+#インポート
+from screenshot import get_screenshot
+from draw import draw_frequent_word
+
 
 # 特定のワードを受け取りそのワードが含まれている要素を返す関数(チェックするワード, driver)
 def get_element_byword(check_word, driver):
@@ -25,17 +32,14 @@ def get_element_byword(check_word, driver):
     return tags
 
 # URLを受け取り出現単語を出現数が多い単語順に並び替えて辞書型で出力する関数(url)
-def get_frequent_words(url):
-    ##HTML取得・整形
-    html=requests.get(url).text
-    soup=BeautifulSoup(html,"html.parser")
+def get_frequent_words(html):
 
     ##scriptやstyleを含む要素を削除する
-    for script in soup(["script", "style"]):
+    for script in html(["script", "style"]):
         script.decompose()
 
     ##テキストのみを取得=タグは全部取る
-    text=soup.get_text()
+    text=html.get_text()
 
     ##textを改行ごとにリストに入れて、リスト内の要素の前後の空白を削除
     lines= [line.strip() for line in text.splitlines()]
@@ -72,25 +76,48 @@ if __name__ == '__main__':
     binary.add_command_line_options('-headless')
     driver = webdriver.Firefox(firefox_binary=binary)
     driver.get(URL)
+    soup_html = BeautifulSoup( driver.page_source, "html.parser")
 
-    words = get_frequent_words(URL)
-    i = 0
+    ## スクリーンショットの取得
+    get_screenshot(driver)
+
+    ## 頻出単語の一覧を取得
+    words = get_frequent_words(soup_html)
+    print(words)
+
+    ## csvの読み込み・設定
+    print("Load and create CSV file...")
+    tag_list_freq_word = open('./working/tag_list_freq_word.csv', 'w')
+    csvWriter = csv.writer(tag_list_freq_word)
+    csvWriter.writerow(['tag_name', 'level','start_x', 'start_y', 'size_w', 'size_h'])
+
+
+
+    level = 0
     for word, count in words.most_common(): ##most_common関数で出現頻度が高い順に取得
-        if i>=3: break
-        i+=1
+        if level>=3: break
+        level+=1
 
         print("/"*50)
         print("調査対象：%s (%d個)"%(word, count))
 
         tags = get_element_byword(word, driver)
 
-        j = 0
         for tag in tags:
-            tag_name = tag.tag_name
-            tag_id = tag.get_attribute('id')
-            tag_class = tag.get_attribute('class')
-            print("No.%d: <%s> id=\"%s\" class=\"%s\""%(j, tag_name, tag_id, tag_class))
-            j+=1
+            if str(tag.is_displayed()) == "True": #表示されている時のみリストに挿入
+                tag_name = tag.tag_name
+                start_x = tag.location['x']
+                start_y = tag.location['y']
+                size_w = tag.size['width']
+                size_h = tag.size['height']
 
-    # Close Web Browser
+                csvWriter.writerow([tag_name, level, start_x, start_y, size_w, size_h])
+
+    ## CSVファイルクローズ
+    tag_list_freq_word.close()
+
+    ## Close Web Browser
     driver.quit()
+
+    ## 頻出単語の描写
+    draw_frequent_word()
